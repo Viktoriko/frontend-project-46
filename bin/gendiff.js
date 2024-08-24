b31e7fc45df92e6d970150dfa066551e719548b9
Viktoriko <viktoriko18@gmail.com>
2024-08-24 17:41:39 +0300

    feat: task 2. Add arguments and option

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,6 +5,8 @@ const program = new Command();
 
 program
   .description('Compares two configuration files and shows a difference.')
-  .version('0.0.1');
-
-program.parse();
+  .version('0.0.1')
+  .argument('<filepath1>', 'the path to the file being compared')
+  .argument('<filepath2>', 'the path to the file being compared with')
+  .option('-f, --format <type>', 'output format')
+  .parse();
